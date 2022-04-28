460270b9697314bea4fd14643d7c3b1ff4703802
bvujovic <bv.net@outlook.com>
2022-04-28 20:23:16 +0200

    Add project files.

---
+++ TaggedWorld/Tag.cs
@@ -0,0 +1,18 @@
+﻿namespace TaggedWorld
+{
+    /// <summary>
+    /// Tag/oznaka/labela. Reč ili fraza preko koje se tagovani objekat pronalazi.
+    /// </summary>
+    public class Tag
+    {
+        public Tag(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; } = string.Empty;
+
+        public override string ToString()
+            => Name;
+    }
+}
---
+++ TaggedWorld/Target.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TaggedWorld
+{
+    /// <summary>
+    /// Element, Object - objekat koji se taguje.
+    /// </summary>
+    public class Target
+    {
+        // tip: veb link, fajl, folder - enum ili kao [obavezni] tag?
+    }
+}
